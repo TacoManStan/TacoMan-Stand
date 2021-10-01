922d7860e76a80c4bf29c8f1400aeb624b3160af
Spencer Cox <spencercox@outlook.com>
2021-10-01 14:26:04 -0500

    Update ExamplePage.java

--- src/main/java/com/taco/suit_lady/view/ui/ui_internal/pages/ExamplePage.java
+++ src/main/java/com/taco/suit_lady/view/ui/ui_internal/pages/ExamplePage.java
@@ -3,7 +3,6 @@ package com.taco.suit_lady.view.ui.ui_internal.pages;
 import com.taco.suit_lady.view.ui.UINode;
 import com.taco.suit_lady.view.ui.UIPage;
 import com.taco.suit_lady.view.ui.ui_internal.controllers.ExampleSidebarController;
-import com.taco.util.quick.ConsoleBB;
 
 public class ExamplePage extends UIPage<ExampleSidebarController>
 {
@@ -15,13 +14,9 @@ public class ExamplePage extends UIPage<ExampleSidebarController>
 		
 		this.color = color;
 		
-		ConsoleBB.CONSOLE.print(ConsoleBB.PrintType.HEADER, "ExamplePage Constructor 1");
-		
 		final ExampleSidebarController pageController = this.weaver().loadController(ExampleSidebarController.class);
 		pageController.setPage(this);
 		setController(pageController);
-		
-		ConsoleBB.CONSOLE.print(ConsoleBB.PrintType.HEADER, "ExamplePage Constructor 2");
 	}
 	
 	public String getColor()
