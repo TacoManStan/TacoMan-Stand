38e8410de918d1ca14fb1174507aac210475b530
Spencer Cox <spencercox@outlook.com>
2021-11-21 15:57:47 -0600

    Update SidebarNodeGroup.java
    - Changed nameProperty from a ReadOnlyStringWrapper to a StringProperty
    - Removed unused and extremely confusing selectedProperty field and related methods

--- src/main/java/com/taco/suit_lady/view/ui/SidebarNodeGroup.java
+++ src/main/java/com/taco/suit_lady/view/ui/SidebarNodeGroup.java
@@ -2,8 +2,11 @@ package com.taco.suit_lady.view.ui;
 
 import com.taco.suit_lady.view.ui.jfx.button.BoundButtonViewGroup;
 import com.taco.suit_lady.view.ui.jfx.button.ButtonViewGroup;
+import com.taco.suit_lady.view.ui.jfx.button.ButtonViewable;
 import com.taco.suit_lady.view.ui.jfx.button.ImageButton;
-import javafx.beans.property.*;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -12,124 +15,117 @@ import javafx.scene.layout.VBox;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class SidebarNodeGroup extends UINodeGroup {
-
-	private final ReentrantLock lock;
-	private final ReadOnlyStringWrapper nameProperty;
-
-	private final Sidebar owner;
-	private final VBox buttonBox;
-	private final Button button;
-
-	private final BoundButtonViewGroup<UINode> nodeButtonGroup;
-
-	private final ReadOnlyBooleanWrapper selectedProperty;
-	private final ReadOnlyObjectWrapper<Runnable> clickResponderProperty;
-
-	public SidebarNodeGroup(Sidebar owner, Button menuButton) {
-		this(owner, menuButton, new StackPane());
-	}
-
-	public SidebarNodeGroup(Sidebar owner, Button menuButton, StackPane contentPane) {
-		super(contentPane);
-
-		this.lock = owner.getLock();
-		this.nameProperty = new ReadOnlyStringWrapper();
-
-		this.owner = owner;
-		this.buttonBox = new VBox();
-		this.button = menuButton;
-
-		this.nodeButtonGroup = new BoundButtonViewGroup<>(getNodes(), lock);
-
-		this.selectedProperty = new ReadOnlyBooleanWrapper();
-		this.clickResponderProperty = new ReadOnlyObjectWrapper<>();
-
-		//
-
-		this.nodeButtonGroup.selectedButtonProperty().addListener((observable, oldButton, newButton) -> getNodeDisplayer().setDisplay(this.nodeButtonGroup.getViewableByButton(newButton)));
-	}
-
-	protected void initialize() {
-		lock.lock();
-		try {
-			List<UINode> nodes = getNodes();
-			for (UINode node : nodes) {
-				final ImageButton imageButton = node.getButtonView();
-				if (imageButton != null)
-					buttonBox.getChildren().add(imageButton.getImagePane());
-				buttonBox.setSpacing(1.0);
-				buttonBox.setAlignment(Pos.TOP_LEFT);
-			}
-			button.setOnAction(event -> select());
-		} finally {
-			lock.unlock();
-		}
-	}
-
-	//<editor-fold desc="Properties">
-
-	public ReadOnlyStringProperty nameProperty() {
-		return nameProperty.getReadOnlyProperty();
-	}
-
-	public String getName() {
-		return nameProperty.get();
-	}
-
-	//
-
-	protected Sidebar getOwner() {
-		return owner;
-	}
-
-	protected VBox getButtonBox() {
-		return buttonBox;
-	}
-
-	protected Button getButton() {
-		return button;
-	}
-
-	//
-
-	public ButtonViewGroup getButtonViewGroup() {
-		return nodeButtonGroup;
-	}
-
-	//
-
-	public void clearSelection() {
-		nodeButtonGroup.clearSelection();
-	}
-
-	//
-
-	public ReadOnlyBooleanProperty selectedProperty() {
-		return selectedProperty.getReadOnlyProperty();
-	}
-
-	public boolean isSelected() {
-		return selectedProperty.get();
-	}
-
-	private void setSelected(boolean selected) {
-		selectedProperty.set(selected);
-	}
-
-	//
-
-	public ReadOnlyObjectProperty<Runnable> clickResponderProperty() {
-		return clickResponderProperty.getReadOnlyProperty();
-	}
-
-	public Runnable getClickResponder() {
-		return clickResponderProperty.get();
-	}
-
-	//</editor-fold>
-
-	protected void select() {
-		owner.setSelectedNodeGroup(this);
-	}
+// TODO - Add BooleanProperty bidirectionally-bound to the selected SidebarNodeGroup of the parent Sidebar, strictly for convenience.
+public class SidebarNodeGroup extends UINodeGroup
+{
+    private final ReentrantLock lock;
+    private final StringProperty nameProperty;
+    
+    private final Sidebar owner;
+    private final VBox buttonBox;
+    private final Button button;
+    
+    private final BoundButtonViewGroup<UINode> nodeButtonGroup;
+    
+    public SidebarNodeGroup(Sidebar owner, Button menuButton)
+    {
+        this(owner, menuButton, null);
+    }
+    
+    public SidebarNodeGroup(Sidebar owner, Button menuButton, StackPane contentPane)
+    {
+        super(contentPane);
+        
+        this.lock = owner.getLock();
+        this.nameProperty = new SimpleStringProperty();
+        
+        this.owner = owner;
+        this.buttonBox = new VBox();
+        this.button = menuButton;
+        
+        this.nodeButtonGroup = new BoundButtonViewGroup<>(getNodes(), lock);
+        
+        //
+        
+        this.nodeButtonGroup.selectedButtonProperty().addListener((observable, oldButton, newButton) -> getNodeDisplayer().setDisplay(this.nodeButtonGroup.getViewableByButton(newButton)));
+    }
+    
+    protected void initialize()
+    {
+        lock.lock();
+        try {
+            buttonBox.setSpacing(1.0);
+            buttonBox.setAlignment(Pos.TOP_LEFT);
+            
+            final List<UINode> nodes = getNodes();
+            for (UINode node: nodes) {
+                final ImageButton imageButton = node.getButtonView();
+                if (imageButton != null)
+                    buttonBox.getChildren().add(imageButton.getImagePane());
+            }
+            button.setOnAction(event -> select());
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    //<editor-fold desc="Properties">
+    
+    public StringProperty nameProperty()
+    {
+        return nameProperty;
+    }
+    
+    public String getName()
+    {
+        return nameProperty().get();
+    }
+    
+    public void setName(String name)
+    {
+        nameProperty().set(name);
+    }
+    
+    //
+    
+    /**
+     * <p>Returns the {@link Sidebar} object containing this {@link SidebarNodeGroup} instance.</p>
+     *
+     * @return The {@link Sidebar} object containing this {@link SidebarNodeGroup} instance.
+     */
+    public Sidebar getOwner()
+    {
+        return owner;
+    }
+    
+    public VBox getButtonBox()
+    {
+        return buttonBox;
+    }
+    
+    public Button getButton()
+    {
+        return button;
+    }
+    
+    //
+    
+    public BoundButtonViewGroup<UINode> getButtonViewGroup()
+    {
+        return nodeButtonGroup;
+    }
+    
+    //
+    
+    public void clearSelection()
+    {
+        getButtonViewGroup().clearSelection();
+    }
+    
+    //</editor-fold>
+    
+    public void select()
+    {
+        getOwner().setSelectedNodeGroup(this);
+    }
 }
