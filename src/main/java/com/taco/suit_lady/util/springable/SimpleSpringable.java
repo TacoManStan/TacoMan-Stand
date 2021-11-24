e6a7977a22942bd75a82fbb0b419ce794a661484
Spencer Cox <spencercox@outlook.com>
2021-11-24 14:03:42 -0600

    Update SimpleSpringable and StrictSpringable Documentation

--- src/main/java/com/taco/suit_lady/util/springable/SimpleSpringable.java
+++ src/main/java/com/taco/suit_lady/util/springable/SimpleSpringable.java
@@ -1,7 +1,46 @@
 package com.taco.suit_lady.util.springable;
 
 import net.rgielen.fxweaver.core.FxWeaver;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * <p>A basic implementation of {@link Springable}.</p>
+ * <p><b>Details</b></p>
+ * <ol>
+ *     <li>Stores an {@link FxWeaver} and an {@link ConfigurableApplicationContext Application Context} as fields.</li>
+ *     <li>Both {@link FxWeaver} and {@link ConfigurableApplicationContext Application Context} are permitted to be {@code null}.</li>
+ *     <li>To construct a {@link SimpleSpringable} that does not permit {@code null} elements, refer to {@link StrictSpringable}.</li>
+ *     <li>Neither fields wrapped by a {@link SimpleSpringable} can be modified.</li>
+ * </ol>
+ *
+ * @see Springable
+ * @see StrictSpringable
+ */
 public record SimpleSpringable(FxWeaver weaver, ConfigurableApplicationContext ctx)
-        implements Springable { }
+        implements Springable
+{
+    /**
+     * <p>Constructs a new {@link SimpleSpringable} wrapping the specified {@link FxWeaver} and {@link ConfigurableApplicationContext Application Context}.</p>
+     *
+     * @param weaver The {@link FxWeaver} instance to be wrapped by this {@link SimpleSpringable}.
+     * @param ctx    The {@link ConfigurableApplicationContext} instance to be wrapped by this {@link SimpleSpringable}.
+     */
+    public SimpleSpringable { }
+    
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@inheritDoc}
+     */
+    @Override
+    public @Nullable FxWeaver weaver() { return weaver; }
+    
+    /**
+     * {@inheritDoc}
+     *
+     * @return {@inheritDoc}
+     */
+    @Override
+    public @Nullable ConfigurableApplicationContext ctx() { return ctx; }
+}
