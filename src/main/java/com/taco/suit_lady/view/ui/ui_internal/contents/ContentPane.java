ca39561cd5dbb96e1db5556f883dc6b21f605ae0
Spencer Cox <spencercox@outlook.com>
2021-11-30 09:06:47 -0600

    Reformat ContentPane

--- src/main/java/com/taco/suit_lady/view/ui/ui_internal/contents/ContentPane.java
+++ src/main/java/com/taco/suit_lady/view/ui/ui_internal/contents/ContentPane.java
@@ -9,69 +9,74 @@ import javafx.scene.layout.StackPane;
 import java.util.Objects;
 import java.util.function.BiFunction;
 
-public abstract class ContentPane<T> extends StackPane {
-
-	private final ObjectProperty<T> contentProperty;
-	private final BiFunction<T, T, Pane> onDisplaySwitchListener;
-
-	private Pane currentContentPane;
-
-	public ContentPane(BiFunction<T, T, Pane> onDisplaySwitchListener) {
-		this.contentProperty = new SimpleObjectProperty<>();
-		this.onDisplaySwitchListener = onDisplaySwitchListener;
-
-		this.currentContentPane = null;
-
-		//
-
-		this.contentProperty.addListener((observable, oldContent, newContent) -> switchContent(oldContent, newContent));
-	}
-
-	//<editor-fold desc="Properties">
-
-	public final ObjectProperty<T> contentProperty() {
-		return contentProperty;
-	}
-
-	public final T getContent() {
-		return contentProperty.get();
-	}
-
-	public final void setContent(T newContent) {
-		contentProperty.set(newContent);
-	}
-
-	//
-
-	public final BiFunction<T, T, Pane> getOnDisplaySwitchListener() {
-		return onDisplaySwitchListener;
-	}
-
-	//</editor-fold>
-
-	//<editor-fold desc="Helper Methods">
-
-	private void switchContent(T oldContent, T newContent) {
-		FXTools.get().runFX(() -> {
-			if (!Objects.equals(oldContent, newContent)) {
-				if (currentContentPane != null) {
-					getChildren().remove(currentContentPane);
-					currentContentPane.prefWidthProperty().unbind();
-					currentContentPane.prefHeightProperty().unbind();
-				}
-
-				Pane newContentPane = onDisplaySwitchListener.apply(oldContent, newContent);
-				currentContentPane = newContentPane;
-				if (newContentPane != null) {
-					getChildren().add(newContentPane);
-					newContentPane.prefWidthProperty().bind(widthProperty());
-					newContentPane.prefHeightProperty().bind(heightProperty());
-				}
-			}
-		}, true);
-	}
-
-	//</editor-fold>
+public abstract class ContentPane<T> extends StackPane
+{
+    private final ObjectProperty<T> contentProperty;
+    private final BiFunction<T, T, Pane> onDisplaySwitchListener;
+    
+    private Pane currentContentPane;
+    
+    public ContentPane(BiFunction<T, T, Pane> onDisplaySwitchListener)
+    {
+        this.contentProperty = new SimpleObjectProperty<>();
+        this.onDisplaySwitchListener = onDisplaySwitchListener;
+        
+        this.currentContentPane = null;
+        
+        //
+        
+        this.contentProperty.addListener((observable, oldContent, newContent) -> switchContent(oldContent, newContent));
+    }
+    
+    //<editor-fold desc="Properties">
+    
+    public final ObjectProperty<T> contentProperty()
+    {
+        return contentProperty;
+    }
+    
+    public final T getContent()
+    {
+        return contentProperty.get();
+    }
+    
+    public final void setContent(T newContent)
+    {
+        contentProperty.set(newContent);
+    }
+    
+    //
+    
+    public final BiFunction<T, T, Pane> getOnDisplaySwitchListener()
+    {
+        return onDisplaySwitchListener;
+    }
+    
+    //</editor-fold>
+    
+    //<editor-fold desc="Helper Methods">
+    
+    private void switchContent(T oldContent, T newContent)
+    {
+        FXTools.get().runFX(() -> {
+            if (!Objects.equals(oldContent, newContent)) {
+                if (currentContentPane != null) {
+                    getChildren().remove(currentContentPane);
+                    currentContentPane.prefWidthProperty().unbind();
+                    currentContentPane.prefHeightProperty().unbind();
+                }
+                final Pane newContentPane = onDisplaySwitchListener.apply(oldContent, newContent);
+                currentContentPane = newContentPane;
+                if (newContentPane != null) {
+                    getChildren().add(newContentPane);
+                    newContentPane.prefWidthProperty().bind(widthProperty());
+                    newContentPane.prefHeightProperty().bind(heightProperty());
+                }
+            }
+        }, true);
+    }
+    
+    //</editor-fold>
 }
 
 /*
