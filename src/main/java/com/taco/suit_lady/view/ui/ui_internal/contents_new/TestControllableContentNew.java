732c258bc19770a9c9aee8b88e321d4fc28a06af
Spencer Cox <spencercox@outlook.com>
2021-11-30 16:55:08 -0600

    Update TestControllableContentNew.java
    - Removed debug print statement

--- src/main/java/com/taco/suit_lady/view/ui/ui_internal/contents_new/TestControllableContentNew.java
+++ src/main/java/com/taco/suit_lady/view/ui/ui_internal/contents_new/TestControllableContentNew.java
@@ -1,7 +1,6 @@
 package com.taco.suit_lady.view.ui.ui_internal.contents_new;
 
 import com.taco.suit_lady.util.springable.Springable;
-import com.taco.util.quick.ConsoleBB;
 import org.jetbrains.annotations.NotNull;
 
 public class TestControllableContentNew extends ControllableContentNew<TestContentNewController>
@@ -12,9 +11,7 @@ public class TestControllableContentNew extends ControllableContentNew<TestConte
     }
     
     @Override
-    protected void onSet() {
-        ConsoleBB.CONSOLE.print("TestControllableContentNew has been set.");
-    }
+    protected void onSet() { }
     
     @Override
     protected void onRemoved() { }
