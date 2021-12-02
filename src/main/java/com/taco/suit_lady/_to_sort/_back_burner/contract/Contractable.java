9a1b07c4f61f035a8f0035a76391a58a9ce93cf1
Spencer Cox <spencercox@outlook.com>
2021-12-02 12:46:23 -0600

    Added Contractable and Contracts Classes to back_burner package

---
+++ src/main/java/com/taco/suit_lady/_to_sort/_back_burner/contract/Contractable.java
@@ -0,0 +1,9 @@
+package com.taco.suit_lady._to_sort._back_burner.contract;
+
+/**
+ * <p>Allows implementing classes to define an array of {@link String contracts} that the implementation promises to adhere to.</p>
+ */
+public interface Contractable
+{
+    String[] getContracts();
+}
