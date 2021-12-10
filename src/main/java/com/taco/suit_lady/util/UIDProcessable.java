07b25cbe9ee9a613f0a9ac83ca6cbbfcfc838470
Spencer Cox <spencercox@outlook.com>
2021-12-10 14:43:21 -0600

    Update UIDProcessable.java
    - Started adding documentation

--- src/main/java/com/taco/suit_lady/util/UIDProcessable.java
+++ src/main/java/com/taco/suit_lady/util/UIDProcessable.java
@@ -1,16 +1,42 @@
 package com.taco.suit_lady.util;
 
-public interface UIDProcessable extends UID {
+import com.taco.suit_lady.view.ui.jfx.lists.treehandler.TreeCellData;
 
-	UIDProcessor getUIDProcessor();
-
-	@Override
-	default String getUID(Object... params) {
-		return getUIDProcessor().getUID(params);
-	}
-
-	@Override
-	default String getGroupID() {
-		return getUIDProcessor().getGroupID();
-	}
+/**
+ * <p>TO-DOC</p>
+ */
+public interface UIDProcessable extends UID
+{
+    
+    /**
+     * <p>Returns the {@link UIDProcessor} instance for this {@link UIDProcessable} object.</p>
+     * <br><hr><br>
+     * <h2>Example Implementation</h2>
+     * <pre>{@code @Override
+     * public UIDProcessor getUIDProcessor()
+     * {
+     *     if (uIDContainer == null) // Lazy Initialization
+     *         uIDContainer = new UIDProcessor("group-name");
+     *     return uIDContainer;
+     * }}</pre>
+     * <p><b>Details</b></p>
+     * <ol>
+     * <li>In most cases, it is recommended that {@link UIDProcessor} objects are stored as lazy-initialized singleton field variables as shown in the example above.</li>
+     * </ol>
+     *
+     * @return The {@link UIDProcessor} instance for this {@link UIDProcessable} object.
+     */
+    UIDProcessor getUIDProcessor();
+    
+    @Override
+    default String getUID(Object... params)
+    {
+        return getUIDProcessor().getUID(params);
+    }
+    
+    @Override
+    default String getGroupID()
+    {
+        return getUIDProcessor().getGroupID();
+    }
 }
