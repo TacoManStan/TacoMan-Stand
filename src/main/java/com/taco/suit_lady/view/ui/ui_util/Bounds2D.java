7d53bebe4705703801302be8f6df9203a20ba121
Spencer Cox <spencercox@outlook.com>
2021-12-09 18:42:19 -0600

    Update Bounds2D.java
    - Added static conversion methods for converting various existing boundary-valid objects into a Bounds2D instances

--- src/main/java/com/taco/suit_lady/view/ui/ui_util/Bounds2D.java
+++ src/main/java/com/taco/suit_lady/view/ui/ui_util/Bounds2D.java
@@ -1,33 +1,63 @@
 package com.taco.suit_lady.view.ui.ui_util;
 
+import com.taco.suit_lady.util.tools.ExceptionTools;
 import javafx.geometry.BoundingBox;
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
+import org.jetbrains.annotations.NotNull;
 
 public record Bounds2D(int x, int y, int width, int height)
 {
-    public java.awt.Rectangle asAWT()
+    //<editor-fold desc="--- CONVERSION ---">
+    
+    public @NotNull java.awt.Rectangle asAWT()
     {
         return new java.awt.Rectangle(x, y, width, height);
     }
     
-    public javafx.scene.shape.Rectangle asFX()
+    public @NotNull javafx.scene.shape.Rectangle asFX()
     {
         return new javafx.scene.shape.Rectangle(x, y, width, height);
     }
     
-    public Bounds asBounds()
+    public @NotNull Bounds asBounds()
     {
         return new BoundingBox(x, y, width, height);
     }
     
-    public Point2D getLocation()
+    public @NotNull Point2D getLocation()
     {
         return new Point2D(x, y);
     }
     
-    public Point2D getDimensions()
+    public @NotNull Point2D getDimensions()
     {
         return new Point2D(width, height);
     }
+    
+    //</editor-fold>
+    
+    //<editor-fold desc="--- STATIC CONVERSION ---">
+    
+    public static @NotNull Bounds2D fromRectAWT(@NotNull java.awt.Rectangle rectangle)
+    {
+        ExceptionTools.nullCheck(rectangle, "AWT Rectangle");
+        return new Bounds2D(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    }
+    
+    public static @NotNull Bounds2D fromRectFX(@NotNull javafx.scene.shape.Rectangle rectangle)
+    {
+        ExceptionTools.nullCheck(rectangle, "JFX Rectangle");
+        return new Bounds2D((int) rectangle.getX(), (int) rectangle.getY(), (int) rectangle.getWidth(), (int) rectangle.getHeight());
+    }
+    
+    public static @NotNull Bounds2D fromPoints(@NotNull Point2D location, @NotNull Point2D dimensions)
+    {
+        ExceptionTools.nullCheck(location, "Location Point2D");
+        ExceptionTools.nullCheck(dimensions, "Dimensions Point2D");
+        
+        return new Bounds2D((int) location.getX(), (int) location.getY(), (int) dimensions.getX(), (int) dimensions.getY());
+    }
+    
+    //</editor-fold>
 }
