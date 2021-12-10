d745054ce658f158c77adeb47bde329502774428
Spencer Cox <spencercox@outlook.com>
2021-12-10 16:43:03 -0600

    Update UIDProcessable.java
    - Update documentation formatting

--- src/main/java/com/taco/suit_lady/util/UIDProcessable.java
+++ src/main/java/com/taco/suit_lady/util/UIDProcessable.java
@@ -7,10 +7,9 @@ import com.taco.suit_lady.view.ui.jfx.lists.treehandler.TreeCellData;
  */
 public interface UIDProcessable extends UID
 {
-    
     /**
      * <p>Returns the {@link UIDProcessor} instance for this {@link UIDProcessable} object.</p>
-     * <br><hr><br>
+     * <hr><br>
      * <h4>Example Implementation</h4>
      * <pre>{@code @Override
      * public UIDProcessor getUIDProcessor()
@@ -19,9 +18,10 @@ public interface UIDProcessable extends UID
      *         uIDContainer = new UIDProcessor("group-name");
      *     return uIDContainer;
      * }}</pre>
-     * <p><b>Details</b></p>
+     * <br>
+     * <h4>Details</h4>
      * <ol>
-     * <li>In most cases, it is recommended that {@link UIDProcessor} objects are stored as lazy-initialized singleton field variables as shown in the example above.</li>
+     *     <li>In most cases, it is recommended that {@link UIDProcessor} objects are stored as lazy-initialized singleton field variables as shown in the example above.</li>
      * </ol>
      *
      * @return The {@link UIDProcessor} instance for this {@link UIDProcessable} object.
