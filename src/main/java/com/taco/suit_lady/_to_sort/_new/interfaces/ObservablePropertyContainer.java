3807839e5def839fc798afb29e797a48f8731b8e
Spencer Cox <spencercox@outlook.com>
2021-12-09 18:00:46 -0600

    Create ObservablePropertyContainer.java

---
+++ src/main/java/com/taco/suit_lady/_to_sort/_new/interfaces/ObservablePropertyContainer.java
@@ -0,0 +1,9 @@
+package com.taco.suit_lady._to_sort._new.interfaces;
+
+import javafx.beans.Observable;
+
+@FunctionalInterface
+public interface ObservablePropertyContainer
+{
+    Observable[] observableProperties();
+}
