298925f2dc3b9f9aef354a1085abc5a31eaacba4
Spencer Cox <spencercox@outlook.com>
2021-12-18 09:53:35 -0600

    Update FXTools.java
    - Added FXTools.Colors inner static utility class

--- src/main/java/com/taco/suit_lady/util/tools/fx_tools/FXTools.java
+++ src/main/java/com/taco/suit_lady/util/tools/fx_tools/FXTools.java
@@ -2141,6 +2141,18 @@ public class FXTools
         }
     }
     
+    public static class Colors {
+        private Colors() { } // No Instance
+        
+        public static Color from255(int r, int g, int b) {
+            return from255(r, g, b, 255);
+        }
+        
+        public static Color from255(int r, int g, int b, int a) {
+            return Color.color(r / 255.0, g / 255.0, b / 255.0, a / 255.0);
+        }
+    }
+    
     //</editor-fold>
 }
 
