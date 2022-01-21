57a84249460b938d2e05f10713102b7951a9ffd3
Spencer Cox <spencercox@outlook.com>
2022-01-21 11:25:46 -0600

    Update Boundable.java & BoundsBinding.java
    Boundable
        - Added additional convenience default methods
        - Added documentation
        - Added inner LocationDefinition enum for use with new getLocation(LocationDefinition) method
    
    BoundsBinding
        - Added several convenience bindings for safe access to width, height, x, and y values.
        - Added documentation to overwritten Binding methods
        - Other minor changes

--- src/main/java/com/taco/suit_lady/ui/jfx/util/Boundable.java
+++ src/main/java/com/taco/suit_lady/ui/jfx/util/Boundable.java
@@ -1,7 +1,7 @@
 package com.taco.suit_lady.ui.jfx.util;
 
 import javafx.geometry.Point2D;
-import org.docx4j.wml.P;
+import org.jetbrains.annotations.Nullable;
 
 public interface Boundable {
     
@@ -14,9 +14,44 @@ public interface Boundable {
     //<editor-fold desc="--- DEFAULT ---">
     
     default Bounds getBounds() { return new Bounds(getX(), getY(), getWidth(), getHeight()); }
-    
     default Point2D getDimensions() { return new Point2D(getWidth(), getHeight()); }
-    default Point2D getLocation() { return new Point2D(getX(), getY()); }
+    
+    
+    default int getMinX() { return getX(); }
+    default int getMaxX() { return getMinX() + getWidth(); }
+    default int getMinY() { return getY(); }
+    default int getMaxY() { return getMinY() + getHeight(); }
+    default double getMidX() { return getMinX() + (getWidth() / 2d); }
+    default double getMidY() { return getMinY() + (getHeight() / 2d); }
+    
+    
+    /**
+     * <p>Returns a {@link Point2D} object representing a coordinate point on this {@link Boundable} instance, defined by the specified {@link LocationDefinition LocationDefinition}.</p>
+     * <p><b>Details</b></p>
+     * <ol>
+     *     <li>If the specified {@link LocationDefinition LocationDefinition} is {@code null}, the {@link LocationDefinition#getDefault() default} value — <i>{@link LocationDefinition#TOP_LEFT LocationDefinition.TOP_LEFT}</i>, as defined by <i>{@link LocationDefinition#getDefault() LocationDefinition.getDefault()}</i> — is used instead.</li>
+     * </ol>
+     *
+     * @param locationDefinition The {@link LocationDefinition LocationDefinition} enum value representing which {@link Point2D point} on this {@link Boundable} should be returned.
+     *
+     * @return A {@link Point2D} object representing a coordinate point on this {@link Boundable} instance, defined by the specified {@link LocationDefinition LocationDefinition}.
+     */
+    default Point2D getLocation(@Nullable LocationDefinition locationDefinition) {
+        locationDefinition = locationDefinition != null ? locationDefinition : LocationDefinition.getDefault();
+        switch (locationDefinition) {
+            default -> new Point2D(getMinX(), getMinY());
+            
+            case TOP_RIGHT -> new Point2D(getMaxX(), getMinY());
+            case BOTTOM_LEFT -> new Point2D(getMinX(), getMaxY());
+            case BOTTOM_RIGHT -> new Point2D(getMaxX(), getMaxY());
+            
+            case CENTER -> new Point2D(getMidX(), getMidY());
+            case CENTER_LEFT -> new Point2D(getMinX(), getMidY());
+            case CENTER_RIGHT -> new Point2D(getMaxX(), getMidY());
+            case CENTER_TOP -> new Point2D(getMidX(), getMinY());
+            case CENTER_BOTTOM -> new Point2D(getMidX(), getMaxY());
+        }
+    }
     
     //<editor-fold desc="> Safe/Fallback Accessors">
     
@@ -49,4 +84,19 @@ public interface Boundable {
     //</editor-fold>
     
     //</editor-fold>
+    
+    /**
+     * <p>An {@code enum} that defines a {@link Point2D point} on this {@link Boundable}.</p>
+     * <p><b>Details</b></p>
+     * <ol>
+     *     <li>Used in calculation methods such as <i>{@link #getLocation(LocationDefinition) getLocation(LocationType)}</i>.</li>
+     * </ol>
+     */
+    enum LocationDefinition {
+        TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT,
+        CENTER, CENTER_LEFT, CENTER_RIGHT, CENTER_TOP, CENTER_BOTTOM
+        
+        public static LocationDefinition getDefault() { return LocationDefinition.TOP_LEFT; }
+        
+        }
 }
--- src/main/java/com/taco/suit_lady/ui/jfx/util/BoundsBinding.java
+++ src/main/java/com/taco/suit_lady/ui/jfx/util/BoundsBinding.java
@@ -4,6 +4,7 @@ import com.taco.suit_lady.util.tools.PropertyTools;
 import javafx.beans.InvalidationListener;
 import javafx.beans.binding.Binding;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.IntegerBinding;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -19,7 +20,13 @@ public class BoundsBinding
     private final IntegerProperty widthProperty;
     private final IntegerProperty heightProperty;
     
+    
     private final ObjectBinding<Bounds> boundsBinding;
+    
+    private final IntegerBinding xSafeBinding;
+    private final IntegerBinding ySafeBinding;
+    private final IntegerBinding widthSafeBinding;
+    private final IntegerBinding heightSafeBinding;
     
     public BoundsBinding() {
         this.xProperty = new SimpleIntegerProperty();
@@ -27,78 +34,39 @@ public class BoundsBinding
         this.widthProperty = new SimpleIntegerProperty();
         this.heightProperty = new SimpleIntegerProperty();
         
+        
         this.boundsBinding = Bindings.createObjectBinding(
                 () -> new Bounds(getX(), getY(), getWidth(), getHeight()),
                 xProperty, yProperty, widthProperty, heightProperty);
+        
+        this.xSafeBinding = Bindings.createIntegerBinding(() -> getSafeX(), xProperty);
+        this.ySafeBinding = Bindings.createIntegerBinding(() -> getSafeY(), yProperty);
+        this.widthSafeBinding = Bindings.createIntegerBinding(() -> getSafeWidth(), widthProperty);
+        this.heightSafeBinding = Bindings.createIntegerBinding(() -> getSafeHeight(), heightProperty);
     }
     
     //<editor-fold desc="--- PROPERTIES ---">
     
-    public final IntegerProperty xProperty() {
-        return xProperty;
-    }
+    public final IntegerProperty xProperty() { return xProperty; }
+    @Override public final int getX() { return xProperty.get(); }
+    public final int setX(int newValue) { return PropertyTools.setProperty(xProperty, newValue); }
     
-    @Override
-    public final int getX() {
-        return xProperty.get();
-    }
+    public final IntegerProperty yProperty() { return yProperty; }
+    @Override public final int getY() { return yProperty.get(); }
+    public final int setY(int newValue) { return PropertyTools.setProperty(yProperty, newValue); }
     
-    public final int setX(int newValue) {
-        return PropertyTools.setProperty(xProperty, newValue);
-    }
+    public final IntegerProperty widthProperty() { return widthProperty; }
+    @Override public final int getWidth() { return widthProperty.get(); }
+    public final int setWidth(int newValue) { return PropertyTools.setProperty(widthProperty, newValue); }
     
+    public final IntegerProperty heightProperty() { return heightProperty; }
+    @Override public final int getHeight() { return heightProperty.get(); }
+    public final int setHeight(int newValue) { return PropertyTools.setProperty(heightProperty, newValue); }
     
-    public final IntegerProperty yProperty() {
-        return yProperty;
-    }
+    //<editor-fold desc="> Bindings">
     
-    @Override
-    public final int getY() {
-        return yProperty.get();
-    }
-    
-    public final int setY(int newValue) {
-        return PropertyTools.setProperty(yProperty, newValue);
-    }
-    
-    
-    public final IntegerProperty widthProperty() {
-        return widthProperty;
-    }
-    
-    @Override
-    public final int getWidth() {
-        return widthProperty.get();
-    }
-    
-    public final int setWidth(int newValue) {
-        return PropertyTools.setProperty(widthProperty, newValue);
-    }
-    
-    
-    public final IntegerProperty heightProperty() {
-        return heightProperty;
-    }
-    
-    @Override
-    public final int getHeight() {
-        return heightProperty.get();
-    }
-    
-    public final int setHeight(int newValue) {
-        return PropertyTools.setProperty(heightProperty, newValue);
-    }
-    
-    
-    public final ObjectBinding<Bounds> boundsBinding() {
-        return boundsBinding;
-    }
-    
-    @Override
-    public final Bounds getBounds() {
-        return boundsBinding.get();
-    }
-    
+    public final ObjectBinding<Bounds> boundsBinding() { return boundsBinding; }
+    @Override public final Bounds getBounds() { return boundsBinding.get(); }
     public final void setBounds(@NotNull Bounds newValue) {
         setX(newValue.getX());
         setY(newValue.getY());
@@ -106,54 +74,62 @@ public class BoundsBinding
         setHeight(newValue.getHeight());
     }
     
+    public final IntegerBinding xSafeBinding() { return xSafeBinding; }
+    public final IntegerBinding ySafeBinding() { return ySafeBinding; }
+    public final IntegerBinding widthSafeBinding() { return widthSafeBinding; }
+    public final IntegerBinding heightSafeBinding() { return heightSafeBinding; }
+    
+    //</editor-fold>
+    
     //</editor-fold>
     
     //<editor-fold desc="--- IMPLEMENTATIONS ---">
     
-    @Override
-    public final boolean isValid() {
-        return boundsBinding.isValid();
-    }
+    //<editor-fold desc="> Binding">
     
-    @Override
-    public final void invalidate() {
-        boundsBinding.invalidate();
-    }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#getValue() getValue()}</i></p>
+     */
+    @Override public final Bounds getValue() { return boundsBinding.getValue(); }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#getDependencies() getDependencies()}</i></p>
+     */
+    @Override public final ObservableList<?> getDependencies() { return boundsBinding.getDependencies(); }
     
-    @Override
-    public final ObservableList<?> getDependencies() {
-        return boundsBinding.getDependencies();
-    }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#dispose() dispose()}</i></p>
+     */
+    @Override public final void dispose() { boundsBinding.dispose(); }
     
-    @Override
-    public final void dispose() {
-        boundsBinding.dispose();
-    }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#isValid() isValid()}</i></p>
+     */
+    @Override public final boolean isValid() { return boundsBinding.isValid(); }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#invalidate() invalidate()}</i></p>
+     */
+    @Override public final void invalidate() { boundsBinding.invalidate(); }
     
-    @Override
-    public final void addListener(ChangeListener<? super Bounds> listener) {
-        boundsBinding.addListener(listener);
-    }
     
-    @Override
-    public final void removeListener(ChangeListener<? super Bounds> listener) {
-        boundsBinding.removeListener(listener);
-    }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#addListener(ChangeListener) addListener}<b>(</b>{@link ChangeListener}<b>)</b></i></p>
+     */
+    @Override public final void addListener(ChangeListener<? super Bounds> listener) { boundsBinding.addListener(listener); }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#removeListener(ChangeListener) removeListener}<b>(</b>{@link ChangeListener}<b>)</b></i></p>
+     */
+    @Override public final void removeListener(ChangeListener<? super Bounds> listener) { boundsBinding.removeListener(listener); }
     
-    @Override
-    public final Bounds getValue() {
-        return boundsBinding.getValue();
-    }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#addListener(InvalidationListener) addListener}<b>(</b>{@link InvalidationListener}<b>)</b></i></p>
+     */
+    @Override public final void addListener(InvalidationListener listener) { boundsBinding.addListener(listener); }
+    /**
+     * <p><b>Passthrough Definition:</b> <i>{@link #boundsBinding()}<b>.</b>{@link Binding#removeListener(InvalidationListener) removeListener}<b>(</b>{@link InvalidationListener}<b>)</b></i></p>
+     */
+    @Override public final void removeListener(InvalidationListener listener) { boundsBinding.removeListener(listener); }
     
-    @Override
-    public final void addListener(InvalidationListener listener) {
-        boundsBinding.addListener(listener);
-    }
-    
-    @Override
-    public final void removeListener(InvalidationListener listener) {
-        boundsBinding.removeListener(listener);
-    }
+    //</editor-fold>
     
     //</editor-fold>
 }
