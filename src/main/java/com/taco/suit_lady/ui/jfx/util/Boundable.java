c8f8ac1465ea35392c903d41a064af0f6799fd6b
Spencer Cox <spencercox@outlook.com>
2022-01-21 10:52:15 -0600

    Update Boundable.java
    - Added additional convenience default methods

--- src/main/java/com/taco/suit_lady/ui/jfx/util/Boundable.java
+++ src/main/java/com/taco/suit_lady/ui/jfx/util/Boundable.java
@@ -1,5 +1,8 @@
 package com.taco.suit_lady.ui.jfx.util;
 
+import javafx.geometry.Point2D;
+import org.docx4j.wml.P;
+
 public interface Boundable {
     
     int getX();
@@ -10,9 +13,40 @@ public interface Boundable {
     
     //<editor-fold desc="--- DEFAULT ---">
     
-    default Bounds getBounds() {
-        return new Bounds(getX(), getY(), getWidth(), getHeight());
+    default Bounds getBounds() { return new Bounds(getX(), getY(), getWidth(), getHeight()); }
+    
+    default Point2D getDimensions() { return new Point2D(getWidth(), getHeight()); }
+    default Point2D getLocation() { return new Point2D(getX(), getY()); }
+    
+    //<editor-fold desc="> Safe/Fallback Accessors">
+    
+    default int getSafeX() {
+        int x = getX();
+        return x > 0 ? x : fallbackX();
     }
+    default int getSafeY() {
+        int y = getY();
+        return y > 0 ? y : fallbackY();
+    }
+    
+    default int getSafeWidth() {
+        int width = getWidth();
+        return width > 0 ? width : fallbackWidth();
+    }
+    default int getSafeHeight() {
+        int height = getHeight();
+        return height > 0 ? height : fallbackHeight();
+    }
+    
+    //
+    
+    default int fallbackX() { return 1; }
+    default int fallbackY() { return 1; }
+    
+    default int fallbackWidth() { return 1; }
+    default int fallbackHeight() { return 1; }
+    
+    //</editor-fold>
     
     //</editor-fold>
 }
