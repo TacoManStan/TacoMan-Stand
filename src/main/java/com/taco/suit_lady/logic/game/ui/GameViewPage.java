5c52dfbcf1b32cadfcf583ef00cd25a7b2585dce
Spencer Cox <spencercox@outlook.com>
2022-01-24 10:28:14 -0600

    Update GameViewPage.java
    - Reformatted

--- src/main/java/com/taco/suit_lady/logic/game/ui/GameViewPage.java
+++ src/main/java/com/taco/suit_lady/logic/game/ui/GameViewPage.java
@@ -4,21 +4,16 @@ import com.taco.suit_lady.ui.UIBook;
 import com.taco.suit_lady.ui.UIPage;
 import org.jetbrains.annotations.NotNull;
 
-public class GameViewPage extends UIPage<GameViewPageController>
-{
+public class GameViewPage extends UIPage<GameViewPageController> {
+    
     private final GameViewContent slContent;
     
-    public GameViewPage(@NotNull UIBook owner, @NotNull GameViewContent slContent, Object... constructorParams)
-    {
+    public GameViewPage(@NotNull UIBook owner, @NotNull GameViewContent slContent, Object... constructorParams) {
         super(owner, constructorParams);
-        
         this.slContent = slContent;
     }
     
-    public GameViewContent getSLContent()
-    {
-        return slContent;
-    }
+    public GameViewContent getSLContent() { return slContent; }
     
     @Override
     protected void initializePage(@NotNull Object[] constructorParams) {
@@ -26,8 +21,7 @@ public class GameViewPage extends UIPage<GameViewPageController>
     }
     
     @Override
-    protected @NotNull Class<GameViewPageController> controllerDefinition()
-    {
+    protected @NotNull Class<GameViewPageController> controllerDefinition() {
         return GameViewPageController.class;
     }
 }
