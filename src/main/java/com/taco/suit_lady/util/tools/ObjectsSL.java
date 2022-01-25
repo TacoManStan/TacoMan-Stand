c35efe347f2e4212627fa298c649f97b08d25a4b
Spencer Cox <spencercox@outlook.com>
2022-01-25 12:12:23 -0600

    Update ObjectsSL.java
    - Added doIf(...) utility methods

--- src/main/java/com/taco/suit_lady/util/tools/ObjectsSL.java
+++ src/main/java/com/taco/suit_lady/util/tools/ObjectsSL.java
@@ -1,47 +1,47 @@
 package com.taco.suit_lady.util.tools;
 
-import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ObjectsSL
-{
-    public static boolean equals(Object obj1, Object obj2)
-    {
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class ObjectsSL {
+    private ObjectsSL() { } //No Instance
+    
+    //<editor-fold desc="--- EQUALS ---">
+    
+    public static boolean equals(Object obj1, Object obj2) {
         return Objects.equals(obj1, obj2);
     }
     
-    public static boolean equalsExcludeNull(Object obj1, Object obj2)
-    {
+    public static boolean equalsExcludeNull(Object obj1, Object obj2) {
         return obj1 != null && obj2 != null && Objects.equals(obj1, obj2);
     }
     
     //
     
-    public static boolean equalsAny(Object obj, Object... objs)
-    {
+    public static boolean equalsAny(Object obj, Object... objs) {
         return equals(obj, false, true, false, objs);
     }
     
-    public static boolean equalsAll(Object obj, Object... objs)
-    {
+    public static boolean equalsAll(Object obj, Object... objs) {
         return equals(obj, true, true, false, objs);
     }
     
-    public static boolean equals(Object obj, boolean requireAll, boolean allowNull, boolean deep, Object... objs)
-    {
+    public static boolean equals(Object obj, boolean requireAll, boolean allowNull, boolean deep, Object... objs) {
         if (objs == null || (!allowNull && obj == null))
             return false;
         else if (objs.length == 1)
             return equalsImpl(obj, objs[0], allowNull, deep);
-        else
-        {
+        else {
             for (Object arrObj: objs)
-                if (equalsImpl(obj, arrObj, allowNull, deep))
-                {
+                if (equalsImpl(obj, arrObj, allowNull, deep)) {
                     if (requireAll)
                         return false;
-                }
-                else
-                {
+                } else {
                     if (!requireAll)
                         return true;
                 }
@@ -49,11 +49,38 @@ public class ObjectsSL
         }
     }
     
-    private static boolean equalsImpl(Object obj1, Object obj2, boolean allowNull, boolean deep)
-    {
+    private static boolean equalsImpl(Object obj1, Object obj2, boolean allowNull, boolean deep) {
         if (allowNull && obj1 == null && obj2 == null)
             return true;
         else
             return deep ? Objects.deepEquals(obj1, obj2) : Objects.equals(obj1, obj2);
     }
+    
+    //</editor-fold>
+    
+    //<editor-fold desc="--- CONDITIONAL OPERATIONS ---">
+    
+    public static <T, R> @Nullable R doIfNonNull(@NotNull Supplier<T> valueFactory, @NotNull Function<T, R> filterPassedOperation) {
+        return doIfNonNull(valueFactory, filterPassedOperation, null);
+    }
+    
+    public static <T, R> @Nullable R doIfNonNull(@NotNull Supplier<T> valueFactory, @NotNull Function<T, R> filterPassedOperation, @Nullable Function<T, R> filterFailedOperation) {
+        return doIf(valueFactory, value -> value != null, filterPassedOperation, filterFailedOperation);
+    }
+    
+    
+    public static <T, R> @Nullable R doIf(@NotNull Supplier<T> valueFactory, @NotNull Predicate<T> filter, @NotNull Function<T, R> filterPassedOperation) {
+        return doIf(valueFactory, filter, filterPassedOperation, null);
+    }
+    
+    public static <T, R> @Nullable R doIf(@NotNull Supplier<T> valueFactory, @NotNull Predicate<T> filter, @NotNull Function<T, R> filterPassedOperation, @Nullable Function<T, R> filterFailedOperation) {
+        T input = valueFactory.get();
+        if (filter.test(input))
+            return filterPassedOperation.apply(input);
+        else if (filterFailedOperation != null)
+            return filterFailedOperation.apply(input);
+        return null;
+    }
+    
+    //</editor-fold>
 }
