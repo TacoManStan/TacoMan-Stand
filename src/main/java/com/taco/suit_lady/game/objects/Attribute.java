dba5434d9392aaeb2f01d7d6b4df93dff9aa3903
Spencer Cox <spencercox@outlook.com>
2022-02-09 11:36:28 -0600

    Create Attribute.java

---
+++ src/main/java/com/taco/suit_lady/game/objects/Attribute.java
@@ -0,0 +1,45 @@
+package com.taco.suit_lady.game.objects;
+
+import com.taco.suit_lady.game.interfaces.GameComponent;
+import com.taco.suit_lady.game.ui.GameViewContent;
+import com.taco.suit_lady.util.Lockable;
+import com.taco.suit_lady.util.springable.Springable;
+import com.taco.suit_lady.util.springable.SpringableWrapper;
+import com.taco.suit_lady.util.tools.PropertiesSL;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.locks.Lock;
+
+public class Attribute<T>
+        implements SpringableWrapper, Lockable, GameComponent {
+    
+    private final GameObject owner;
+    
+    private final ObjectProperty<T> valueProperty;
+    
+    public Attribute(@NotNull GameObject owner) {
+        this.owner = owner;
+        this.valueProperty = new SimpleObjectProperty<>();
+    }
+    
+    //<editor-fold desc="--- PROPERTIES ---">
+    
+    public final GameObject getOwner() { return owner; }
+    
+    public final ObjectProperty<T> valueProperty() { return valueProperty; }
+    public final T getValue() { return valueProperty.get(); }
+    public final T setValue(T newValue) { return PropertiesSL.setProperty(valueProperty, newValue); }
+    
+    //</editor-fold>
+    
+    //<editor-fold desc="--- IMPLEMENTATIONS ---">
+    
+    @Override public @NotNull GameViewContent getGame() { return owner.getGame(); }
+    
+    @Override public @NotNull Springable springable() { return owner; }
+    @Override public @NotNull Lock getLock() { return owner.getLock(); }
+    
+    //</editor-fold>
+}
