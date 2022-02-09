9de481e93af7cddacecb4ad9e7de17ffe10e1aa0
Spencer Cox <spencercox@outlook.com>
2022-02-09 12:08:39 -0600

    Continue Work on Attribute.java

--- src/main/java/com/taco/suit_lady/game/objects/Attribute.java
+++ src/main/java/com/taco/suit_lady/game/objects/Attribute.java
@@ -7,6 +7,8 @@ import com.taco.suit_lady.util.springable.Springable;
 import com.taco.suit_lady.util.springable.SpringableWrapper;
 import com.taco.suit_lady.util.tools.PropertiesSL;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleObjectProperty;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,16 +19,24 @@ public class Attribute<T>
     
     private final GameObject owner;
     
+    private final ReadOnlyStringWrapper idProperty;
     private final ObjectProperty<T> valueProperty;
     
     public Attribute(@NotNull GameObject owner) {
         this.owner = owner;
+        
         this.valueProperty = new SimpleObjectProperty<>();
+        this.idProperty = new ReadOnlyStringWrapper();
     }
     
     //<editor-fold desc="--- PROPERTIES ---">
     
     public final GameObject getOwner() { return owner; }
+    
+    
+    public final ReadOnlyStringProperty readOnlyIdProperty() { return idProperty.getReadOnlyProperty(); }
+    public final String getId() { return idProperty.get(); }
+    public final String setId(@NotNull String newValue) { return PropertiesSL.setProperty(idProperty, newValue); }
     
     public final ObjectProperty<T> valueProperty() { return valueProperty; }
     public final T getValue() { return valueProperty.get(); }
---
+++ src/main/java/com/taco/suit_lady/game/objects/AttributeManager.java
@@ -0,0 +1,100 @@
+package com.taco.suit_lady.game.objects;
+
+import com.taco.suit_lady.game.interfaces.GameComponent;
+import com.taco.suit_lady.game.ui.GameViewContent;
+import com.taco.suit_lady.util.Lockable;
+import com.taco.suit_lady.util.springable.Springable;
+import com.taco.suit_lady.util.springable.SpringableWrapper;
+import javafx.beans.property.MapProperty;
+import javafx.beans.property.SimpleMapProperty;
+import javafx.collections.FXCollections;
+import org.docx4j.org.apache.xpath.operations.Bool;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.locks.Lock;
+import java.util.function.Supplier;
+
+public class AttributeManager
+        implements SpringableWrapper, Lockable, GameComponent {
+    
+    private final GameObject owner;
+    
+    private final MapProperty<String, Attribute<?>> attributeMap;
+    
+    public AttributeManager(@NotNull GameObject owner) {
+        this.owner = owner;
+        
+        this.attributeMap = new SimpleMapProperty<>(FXCollections.observableHashMap());
+    }
+    
+    //<editor-fold desc="--- PROPERTIES ---">
+    
+    public final GameObject getOwner() { return owner; }
+    
+    //
+    
+    public final Attribute<?> addAttribute(@Nullable Attribute<?> attribute) {
+        return sync(() -> {
+            if (attribute != null && attribute.getId() != null)
+                if (attributeMap.containsValue(attribute))
+                    System.err.println("WARNING: AttributeManager already contains Attribute [" + attribute + "]");
+                else if (attributeMap.containsKey(attribute.getId()))
+                    System.err.println("WARNING: Attribute with ID \"" + attribute.getId() + "\" is already present in this AttributeManager [" + attributeMap.get(attribute.getId()));
+                else
+                    return attributeMap.put(attribute.getId(), attribute);
+            return null;
+        });
+    }
+    
+    
+    public final <T> Attribute<T> getAttribute(@NotNull String id, @NotNull Class<T> type) { return sync(() -> (Attribute<T>) attributeMap.get(id)); }
+    
+    public final Attribute<Boolean> getBooleanAttribute(@NotNull String id) { return getAttribute(id, Boolean.class); }
+    public final Attribute<Integer> getIntegerAttribute(@NotNull String id) { return getAttribute(id, Integer.class); }
+    public final Attribute<Long> getLongAttribute(@NotNull String id) { return getAttribute(id, Long.class); }
+    public final Attribute<Float> getFloatAttribute(@NotNull String id) { return getAttribute(id, Float.class); }
+    public final Attribute<Double> getDoubleAttribute(@NotNull String id) { return getAttribute(id, Double.class); }
+    public final Attribute<String> getStringAttribute(@NotNull String id) { return getAttribute(id, String.class); }
+    
+    //<editor-fold desc="> Attribute Map Accessors">
+    
+    public final <T> @Nullable T getValue(@NotNull String id, @NotNull Class<T> type, @NotNull Supplier<T> defaultValueSupplier) {
+        final Attribute<T> attribute = getAttribute(id, type);
+        if (attribute != null)
+            return attribute.getValue();
+        return defaultValueSupplier.get();
+    }
+    
+    
+    public final boolean getBooleanValue(@NotNull String id) { return getBooleanValue(id, () -> false); }
+    public final boolean getBooleanValue(@NotNull String id, @NotNull Supplier<Boolean> defaultValueSupplier) { return getValue(id, Boolean.class, defaultValueSupplier); }
+    
+    public final int getIntegerValue(@NotNull String id) { return getIntegerValue(id, () -> -1); }
+    public final int getIntegerValue(@NotNull String id, @NotNull Supplier<Integer> defaultValueSupplier) { return getValue(id, Integer.class, defaultValueSupplier); }
+    
+    public final long getLongValue(@NotNull String id) { return getLongValue(id, () -> -1L); }
+    public final long getLongValue(@NotNull String id, @NotNull Supplier<Long> defaultValueSupplier) { return getValue(id, Long.class, defaultValueSupplier); }
+    
+    public final float getFloatValue(@NotNull String id) { return getFloatValue(id, () -> -1F); }
+    public final float getFloatValue(@NotNull String id, @NotNull Supplier<Float> defaultValueSupplier) { return getValue(id, Float.class, defaultValueSupplier); }
+    
+    public final double getDoubleValue(@NotNull String id) { return getDoubleValue(id, () -> -1D); }
+    public final double getDoubleValue(@NotNull String id, @NotNull Supplier<Double> defaultValueSupplier) { return getValue(id, Double.class, defaultValueSupplier); }
+    
+    public final String getStringValue(@NotNull String id) { return getStringValue(id, () -> ""); }
+    public final String getStringValue(@NotNull String id, @NotNull Supplier<String> defaultValueSupplier) { return getValue(id, String.class, defaultValueSupplier); }
+    
+    //</editor-fold>
+    
+    //</editor-fold>
+    
+    //<editor-fold desc="--- IMPLEMENTATIONS ---">
+    
+    @Override public @NotNull GameViewContent getGame() { return owner.getGame(); }
+    
+    @Override public @NotNull Springable springable() { return owner; }
+    @Override public @NotNull Lock getLock() { return owner.getLock(); }
+    
+    //</editor-fold>
+}
