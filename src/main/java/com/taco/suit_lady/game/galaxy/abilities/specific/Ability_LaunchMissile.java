fbd55f57eb6703ce44adfb3cb58ba432c14bcd27
Spencer Cox <spencercox@outlook.com>
2022-02-24 11:17:06 -0600

    Update Ability_LaunchMissile.java

--- src/main/java/com/taco/suit_lady/game/galaxy/abilities/specific/Ability_LaunchMissile.java
+++ src/main/java/com/taco/suit_lady/game/galaxy/abilities/specific/Ability_LaunchMissile.java
@@ -1,14 +1,13 @@
 package com.taco.suit_lady.game.galaxy.abilities.specific;
 
 import com.taco.suit_lady.game.galaxy.abilities.Ability_TargetEffect;
-import com.taco.suit_lady.game.galaxy.effects.Effect;
 import com.taco.suit_lady.game.galaxy.effects.Effect_LaunchMissile;
 import com.taco.suit_lady.game.objects.GameObject;
 import org.jetbrains.annotations.NotNull;
 
-public class LaunchMissileAbility extends Ability_TargetEffect<Effect_LaunchMissile> {
+public class Ability_LaunchMissile extends Ability_TargetEffect<Effect_LaunchMissile> {
     
-    public LaunchMissileAbility(@NotNull GameObject source) {
+    public Ability_LaunchMissile(@NotNull GameObject source) {
         super(source);
     }
 }
