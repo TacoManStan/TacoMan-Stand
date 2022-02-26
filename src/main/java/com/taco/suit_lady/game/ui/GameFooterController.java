c1b4f8c99f398947046fedfe44e5a5284f859917
Spencer Cox <spencercox@outlook.com>
2022-02-26 11:49:03 -0600

    Minor Update
    Functional Build

--- src/main/java/com/taco/suit_lady/game/ui/GameFooterController.java
+++ src/main/java/com/taco/suit_lady/game/ui/GameFooterController.java
@@ -3,8 +3,14 @@ package com.taco.suit_lady.game.ui;
 import com.taco.suit_lady.ui.FooterController;
 import javafx.scene.layout.Pane;
 import net.rgielen.fxweaver.core.FxWeaver;
+import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@FxmlView("/fxml/game/content/game_view_footer.fxml")
+@Scope("prototype")
 public class GameFooterController
         extends FooterController<GameFooter, GameFooterController, GameViewContent, GameViewContentData, GameViewContentController> {
     
