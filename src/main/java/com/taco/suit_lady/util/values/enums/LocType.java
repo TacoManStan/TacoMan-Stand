294c847dc098d24eca22f8763d9402a5d836ff3e
Spencer Cox <spencercox@outlook.com>
2022-03-10 13:24:45 -0600

    Add Documentation

--- src/main/java/com/taco/suit_lady/util/values/enums/LocType.java
+++ src/main/java/com/taco/suit_lady/util/values/enums/LocType.java
@@ -4,13 +4,17 @@ import com.taco.suit_lady.util.enums.Enumable;
 import com.taco.suit_lady.util.tools.Enu;
 import com.taco.suit_lady.util.tools.Exc;
 import com.taco.suit_lady.util.tools.Exe;
+import com.taco.suit_lady.util.values.numbers.Num;
 import com.taco.suit_lady.util.values.numbers.Num2D;
 import com.taco.suit_lady.util.values.numbers.expressions.NumExpr2D;
+import javafx.scene.control.TreeItem;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public enum LocType
@@ -74,49 +78,30 @@ public enum LocType
     }
     
     /**
-     * <p>Translates the specified {@link Number location} based on the specified {@link Number dimension} and {@link LocType} and then returns the result.</p>
-     *
-     * @param lock          The {@link Lock} instance used to synchronize calculations performed by this method, or {@code null} to perform calculations without synchronization.
-     *                      If {@code allowNullLock} is set to {@code false}, a {@link RuntimeException} is thrown.
-     * @param allowNullLock True if the specified {@link Lock} is permitted to be {@code null}, false if a {@link RuntimeException} should be thrown if the specified {@link Lock} is {@code null}.
-     * @param loc           The x or y location to be translated.
-     *                      Note that the location value must match the axis of the specified dimension value.
-     * @param dim           The width or height dimension value to be used to perform translation.
-     *                      Note that the dimension value must match the axis of the specified location value.
-     * @param locType       The {@link LocType} defining where the specified and translated {@link Number location} values are located relative to the specified {@link Number dimension} value.
-     *
-     * @return The translated value of the specified {@link Number location} based on the specified {@link Number dimension} and {@link LocType}.
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link LocType}<b>.</b>{@link #translate(Lock, boolean, Number, Number, LocType, LocType) translate}<b>(</b><u>lock</u>, <u>allowNullLock</u>, <u>dim</u>, <u>locType</u>, <u>locType</u><b>)</b>
+     * </code></i></blockquote>
      */
     public static double translate(@Nullable Lock lock, boolean allowNullLock, @NotNull Number loc, @NotNull Number dim, @NotNull LocType locType) {
         return translate(lock, allowNullLock, loc, dim, locType, locType);
     }
     
     /**
-     * <p>Translates the specified {@link Number location} based on the specified {@link Number dimension}, {@link LocType Source LocType} and {@link LocType Target LocType} and then returns the result.</p>
-     *
-     * @param loc           The x or y location to be translated.
-     *                      Note that the location value must match the axis of the specified dimension value.
-     * @param dim           The width or height dimension value to be used to perform translation.
-     *                      Note that the dimension value must match the axis of the specified location value.
-     * @param sourceLocType The {@link LocType} defining where the specified {@link Number location} value is located relative to the specified {@link Number dimension} value.
-     * @param targetLocType The {@link LocType} defining where the returned {@link Number location} value is located relative to the specified {@link Number dimension} value.
-     *
-     * @return The translated value of the specified {@link Number location} based on the specified {@link Number dimension}, {@link LocType Source LocType} and {@link LocType Target LocType}.
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link LocType}<b>.</b>{@link #translate(Lock, boolean, Number, Number, LocType, LocType) translate}<b>(</b>null, true, <u>loc</u>, <u>dim</u>, <u>sourceLocType</u>, <u>targetLocType</u><b>)</b>
+     * </code></i></blockquote>
      */
     public static double translate(@NotNull Number loc, @NotNull Number dim, @NotNull LocType sourceLocType, @NotNull LocType targetLocType) {
         return translate(null, true, loc, dim, sourceLocType, targetLocType);
     }
     
     /**
-     * <p>Translates the specified {@link Number location} based on the specified {@link Number dimension} and {@link LocType} and then returns the result.</p>
-     *
-     * @param loc     The x or y location to be translated.
-     *                Note that the location value must match the axis of the specified dimension value.
-     * @param dim     The width or height dimension value to be used to perform translation.
-     *                Note that the dimension value must match the axis of the specified location value.
-     * @param locType The {@link LocType} defining where the specified and translated {@link Number location} values are located relative to the specified {@link Number dimension} value.
-     *
-     * @return The translated value of the specified {@link Number location} based on the specified {@link Number dimension} and {@link LocType}.
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link LocType}<b>.</b>{@link #translate(Number, Number, LocType, LocType) translate}<b>(</b><u>loc</u>, <u>dim</u>, <u>locType</u>, <u>locType</u><b>)</b>
+     * </code></i></blockquote>
      */
     public static double translate(@NotNull Number loc, @NotNull Number dim, @NotNull LocType locType) {
         return translate(loc, dim, locType, locType);
@@ -126,6 +111,16 @@ public enum LocType
     
     //<editor-fold desc="> Location 2D">
     
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #runLocked(Lock, boolean, Supplier) runLocked}<b>(</b><u>lock</u>, <u>allowNullLock</u>, () -> {@link Num2D#Num2D(Number, Number) new} {@link Num2D}<b>(</b>
+     * <br>
+     * --- {@link #translate(Number, Number, LocType, LocType) translate}<b>(</b><u>locX</u>, <u>dimX</u>, <u>sourceLocType</u>, <u>targetLocType</u><b>)</b>,
+     * <br>
+     * --- {@link #translate(Number, Number, LocType, LocType) translate}<b>(</b><u>locY</u>, <u>dimY</u>, <u>sourceLocType</u>, <u>targetLocType</u><b>)))</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@Nullable Lock lock, boolean allowNullLock,
                                            @NotNull Number locX, @NotNull Number locY,
                                            @NotNull Number dimX, @NotNull Number dimY,
@@ -135,20 +130,50 @@ public enum LocType
                 translate(locY, dimY, sourceLocType, targetLocType)));
     }
     
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, Number, Number, Number, Number, LocType, LocType) translate}<b>(</b>null, true, <u>locX</u>, <u>locY</u>, <u>dimX</u>, <u>dimY</u>, <u>sourceLocType</u>, <u>targetLocType</u><b>)</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@NotNull Number locX, @NotNull Number locY,
                                            @NotNull Number dimX, @NotNull Number dimY,
                                            @NotNull LocType sourceLocType, @NotNull LocType targetLocType) {
         return translate(null, true, locX, locY, dimX, dimY, sourceLocType, targetLocType);
     }
+    
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, Number, Number, Number, Number, LocType, LocType) translate}<b>(</b>null, true, <u>locX</u>, <u>locY</u>, <u>dimX</u>, <u>dimY</u>, <u>locType</u>, <u>locType</u><b>)</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@NotNull Number locX, @NotNull Number locY, @NotNull Number dimX, @NotNull Number dimY, @NotNull LocType locType) {
         return translate(null, true, locX, locY, dimX, dimY, locType, locType);
     }
+    
+    
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, Number, Number, Number, Number, LocType, LocType) translate}<b>(</b>null, true, <u>locX</u>, <u>locY</u>, <u>dimX</u>, <u>dimY</u>,
+     * {@link Enu}<b>.</b>{@link Enu#get(Class) get}<b>(</b>{@link LocType}<b>.</b>{@link LocType#getClass() class}<b>)</b>, {@link Enu}<b>.</b>{@link Enu#get(Class) get}<b>(</b>{@link LocType}<b>.</b>{@link LocType#getClass() class}<b>))</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@NotNull Number locX, @NotNull Number locY, @NotNull Number dimX, @NotNull Number dimY) {
         return translate(null, true, locX, locY, dimX, dimY, Enu.get(LocType.class), Enu.get(LocType.class));
     }
     
     //
     
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, Number, Number, Number, Number, LocType, LocType) translate}<b>(</b><u>lock</u>, <u>allowNullLock</u>,
+     * <u>{@link NumExpr2D locs}</u><b>.</b>{@link NumExpr2D#a() a}<b>()</b>, <u>{@link NumExpr2D locs}</u><b>.</b>{@link NumExpr2D#b() b}<b>()</b>,
+     * <u>{@link NumExpr2D dims}</u><b>.</b>{@link NumExpr2D#a() a}<b>()</b>, <u>{@link NumExpr2D dims}</u><b>.</b>{@link NumExpr2D#b() b}<b>()</b>, <u>sourceLocType</u>, <u>targetLocType</u><b>)</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@Nullable Lock lock, boolean allowNullLock,
                                            @NotNull NumExpr2D<?> locs,
                                            @NotNull NumExpr2D<?> dims,
@@ -156,14 +181,36 @@ public enum LocType
         return translate(lock, allowNullLock, locs.a(), locs.b(), dims.a(), dims.b(), sourceLocType, targetLocType);
     }
     
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, NumExpr2D, NumExpr2D, LocType, LocType) translate}<b>(</b>null, true, <u>locs</u>, <u>dims</u>, <u>sourceLocType</u>, <u>targetLocType</u><b>)</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@NotNull NumExpr2D<?> locs,
                                            @NotNull NumExpr2D<?> dims,
                                            @NotNull LocType sourceLocType, @NotNull LocType targetLocType) {
         return translate(null, true, locs, dims, sourceLocType, targetLocType);
     }
+    
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, NumExpr2D, NumExpr2D, LocType, LocType) translate}<b>(</b>null, true, <u>locs</u>, <u>dims</u>, <u>locType</u>, <u>locType</u><b>)</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@NotNull NumExpr2D<?> locs, @NotNull NumExpr2D<?> dims, @NotNull LocType locType) {
         return translate(null, true, locs, dims, locType, locType);
     }
+    
+    /**
+     * <p><b>Passthrough Definition</b></p>
+     * <blockquote><i><code>
+     * {@link #translate(Lock, boolean, NumExpr2D, NumExpr2D, LocType, LocType) translate}<b>(</b>null, true, <u>locs</u>, <u>dims</u>,
+     * {@link Enu}<b>.</b>{@link Enu#get(Class) get}<b>(</b>{@link LocType}<b>.</b>{@link LocType#getClass() class}<b>)</b>,
+     * {@link Enu}<b>.</b>{@link Enu#get(Class) get}<b>(</b>{@link LocType}<b>.</b>{@link LocType#getClass() class}<b>)</b><b>)</b>
+     * </code></i></blockquote>
+     */
     public static @NotNull Num2D translate(@NotNull NumExpr2D<?> locs, @NotNull NumExpr2D<?> dims) {
         return translate(null, true, locs, dims, Enu.get(LocType.class), Enu.get(LocType.class));
     }
