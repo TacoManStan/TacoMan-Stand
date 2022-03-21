02c82aa19fcdac603b9f2462ceff4bc8b93f5957
Spencer Cox <spencercox@outlook.com>
2022-03-21 11:38:23 -0500

    Update Documentation

--- src/main/java/com/taco/suit_lady/game/objects/MapObject.java
+++ src/main/java/com/taco/suit_lady/game/objects/MapObject.java
@@ -1,9 +1,27 @@
 package com.taco.suit_lady.game.objects;
 
+import com.taco.suit_lady.game.GameMap;
 import javafx.beans.binding.NumberExpression;
 import javafx.geometry.Point2D;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * <p>Defines implementing {@link Object Objects} as being a member of a {@link GameMap}.</p>
+ * <p><b>Details</b></p>
+ * <ol>
+ *     <li>The {@code abstract} <i>{@link #xLocationProperty()}</i> and <i>{@link #yLocationProperty()}</i> methods define the {@code Map Location} of this {@link MapObject} instance.</li>
+ *     <li>
+ *         {@link MapObject} also offers several convenience methods. Examples include:
+ *         <ul>
+ *             <li><i>{@link #getLocationX()}</i></li>
+ *             <li><i>{@link #getLocationY()}</i></li>
+ *             <li><i>{@link #getLocation()}</i></li>
+ *             <li><i>{@link #inRange(MapObject, double)}</i></li>
+ *         </ul>
+ *     </li>
+ * </ol>
+ */
+//TO-EXPAND: Examples
 public interface MapObject {
     
     @NotNull NumberExpression xLocationProperty();
