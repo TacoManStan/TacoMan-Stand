ce7f98d9abc5447ba58f207dd98a6420e9d1580e
Spencer Cox <spencercox@outlook.com>
2022-03-21 11:55:47 -0500

    Docs

--- src/main/java/com/taco/suit_lady/game/ui/GameUIData.java
+++ src/main/java/com/taco/suit_lady/game/ui/GameUIData.java
@@ -1,17 +1,34 @@
 package com.taco.suit_lady.game.ui;
 
 import com.taco.suit_lady.game.GameComponent;
+import com.taco.suit_lady.game.GameMap;
 import com.taco.suit_lady.game.objects.tiles.GameTile;
+import com.taco.suit_lady.ui.AppUI;
 import com.taco.suit_lady.util.synchronization.Lockable;
 import com.taco.suit_lady.util.springable.Springable;
 import com.taco.suit_lady.util.springable.SpringableWrapper;
 import com.taco.suit_lady.util.tools.Props;
+import javafx.beans.property.Property;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.locks.Lock;
 
+/**
+ * <p>Contains all {@link Property Properties} pertaining to {@link AppUI UI} values and members.</p>
+ * <p><b>Details</b></p>
+ * <ol>
+ *     <li>The {@link GameUIData} instance for a {@link GameViewContent} object can be accessed using the <i>{@link GameViewContentData#getUIData()}</i> method.</li>
+ *     <li>
+ *         Example {@link GameUIData} Member
+ *         <ul>
+ *             <li>The <i>{@link #selectedTileProperty()}</i> defines the {@link GameTile} currently selected on the currently-visible {@link GameMap} instance.</li>
+ *         </ul>
+ *     </li>
+ * </ol>
+ */
+//TO-EXPAND
 public class GameUIData
         implements SpringableWrapper, Lockable, GameComponent {
     
