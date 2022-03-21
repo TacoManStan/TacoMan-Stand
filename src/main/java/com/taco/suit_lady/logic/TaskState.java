578c9150ddc5d1a8ec11ae38f9919c9614139e48
Spencer Cox <spencercox@outlook.com>
2022-03-21 12:27:30 -0500

    Docs

--- src/main/java/com/taco/suit_lady/logic/TaskState.java
+++ src/main/java/com/taco/suit_lady/logic/TaskState.java
@@ -2,10 +2,14 @@ package com.taco.suit_lady.logic;
 
 import com.taco.suit_lady.util.enums.Enumable;
 
-//TODO: nyi
+/**
+ * <p><b><i>Not Yet Implemented</i></b></p>
+ * <br><hr><br>
+ * <p>Defines the {@link TaskState} of a {@link GameTask} instance.</p>
+ */
+//TO-EXPAND
 public enum TaskState
         implements Enumable<TaskState> {
-    
     PRE_EXECUTION,
     RUNNING,
     PAUSED,
