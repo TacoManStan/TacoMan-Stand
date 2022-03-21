f6d596187451177f011031b9f7419831bbd34f37
Spencer Cox <spencercox@outlook.com>
2022-03-21 10:37:46 -0500

    Update Documentation

--- src/main/java/com/taco/suit_lady/ui/FooterController.java
+++ src/main/java/com/taco/suit_lady/ui/FooterController.java
@@ -7,6 +7,16 @@ import net.rgielen.fxweaver.core.FxWeaver;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * <p>The intermediate {@link Controller} implementation defining the {@code JavaFX Graphics} of the {@link Footer} implementation of type {@link F}.</p>
+ * <p><i>See {@link Footer} for additional information.</i></p>
+ *
+ * @param <F>  The {@link Footer} implementation this {@link FooterController} implementation defines the {@code JavaFX Graphics} of.
+ * @param <FC> This {@link FooterController} implementation.
+ * @param <T>  The {@link Content} implementation the {@link Footer} implementation this {@link FooterController} implementation defines is assigned to.
+ * @param <TD> The {@link ContentData} implementation defining the {@code Logic} of the {@link Content} implementation of type {@link T}.
+ * @param <TC> The {@link ContentController} implementation defining the {@code JavaFX Graphics} of the {@link Content} implementation of type {@link T}.
+ */
 public abstract class FooterController<F extends Footer<F, FC, T, TD, TC>, FC extends FooterController<F, FC, T, TD, TC>,
         T extends Content<T, TD, TC, F, FC>, TD extends ContentData<T, TD, TC, F, FC>, TC extends ContentController<T, TD, TC, F, FC>>
         extends Controller {
