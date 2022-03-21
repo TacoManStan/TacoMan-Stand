fedda0913a0731dec807f2fc6809832581c87dc2
Spencer Cox <spencercox@outlook.com>
2022-03-21 12:26:00 -0500

    Docs

--- src/main/java/com/taco/suit_lady/logic/OneTimeTask.java
+++ src/main/java/com/taco/suit_lady/logic/OneTimeTask.java
@@ -1,12 +1,30 @@
 package com.taco.suit_lady.logic;
 
 import com.taco.suit_lady.game.GameComponent;
+import com.taco.suit_lady.logic.triggers.Galaxy;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * <p>A {@link GameTask} implementation designed to {@link #execute() Execute} only {@code once}.</p>
+ * <p><b>Details</b></p>
+ * <ol>
+ *     <li>To construct a new {@link OneTimeTask} instance, use any of the available {@link OneTimeTask#OneTimeTask(GameComponent, Tickable) Constructors} or applicable {@link Galaxy} {@link Galaxy#newOneTimeTask(GameComponent, Tickable, Runnable) Factory Methods}.</li>
+ * </ol>
+ * <p><i>See {@link TaskManager} for additional information.</i></p>
+ *
+ * @param <E> The type of {@link Tickable} implementation assigned to this {@link OneTimeTask} object.
+ *
+ * @see GameTask
+ * @see TaskManager
+ * @see Galaxy
+ * @see Galaxy#newOneTimeTask(GameComponent, Tickable, Runnable)
+ * @see Galaxy#newOneTimeTask(Tickable, Runnable)
+ */
+//TO-EXPAND: Examples mainly
 public abstract class OneTimeTask<E extends Tickable<E>> extends GameTask<E> {
     
     public OneTimeTask(@NotNull E owner) { super(owner); }
